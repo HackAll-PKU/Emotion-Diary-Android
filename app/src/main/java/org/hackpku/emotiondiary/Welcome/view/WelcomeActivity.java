f570613be84299e02ddd326c83b941cd48f65589
S.Hu <ihsx@qq.com>
2016-05-28 16:21:49 +0800

    设置主题和背景

--- app/src/main/java/org/hackpku/emotiondiary/Welcome/view/WelcomeActivity.java
+++ app/src/main/java/org/hackpku/emotiondiary/Welcome/view/WelcomeActivity.java
@@ -1,8 +1,12 @@
 package org.hackpku.emotiondiary.Welcome.view;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -10,7 +14,7 @@ import org.hackpku.emotiondiary.R;
 import org.hackpku.emotiondiary.Welcome.presenter.IWelcomePresenter;
 import org.hackpku.emotiondiary.Welcome.presenter.WelcomePresenterImpl;
 
-public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener, IWelcomeView {
+public class WelcomeActivity extends Activity implements View.OnClickListener, IWelcomeView {
     private Button btnLogIn;
     private Button btnRecordEmotion;
     private Button btnEnterHomepage;
@@ -20,12 +24,19 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);  //无title
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //全屏
         setContentView(R.layout.activity_welcome);
 
         // 绑定按钮
         btnLogIn = (Button) this.findViewById(R.id.btnLogIn);
         btnRecordEmotion = (Button) this.findViewById(R.id.btnRecordEmotion);
         btnEnterHomepage = (Button) this.findViewById(R.id.btnEnterHomepage);
+
+        btnLogIn.setOnClickListener(this);
+        btnRecordEmotion.setOnClickListener(this);
+        btnEnterHomepage.setOnClickListener(this);
 
         welcomePresenter = new WelcomePresenterImpl(this);
     }
@@ -35,10 +46,13 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()){
             case R.id.btnLogIn:
                 welcomePresenter.doLogIn();
+                break;
             case R.id.btnRecordEmotion:
                 welcomePresenter.recordEmotion();
+                break;
             case R.id.btnEnterHomepage:
                 welcomePresenter.enterHomepage();
+                break;
         }
     }
 
@@ -49,11 +63,25 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
 
     @Override
     public void onRecordEmotion(){
+        //*
         Toast.makeText(this, "Record emotion called", Toast.LENGTH_SHORT).show();
+        /*/
+        Intent intent = new Intent();
+        intent.setClass(WelcomeActivity.this, RecordEmotion.class);
+        startActivity(intent);
+        //finish();
+        //*/
     }
 
     @Override
     public void onEnterHomepage(){
+        //*
         Toast.makeText(this, "Enter homepage called", Toast.LENGTH_SHORT).show();
+        /*/
+        Intent intent = new Intent();
+        intent.setClass(WelcomeActivity.this, Homepage.class);
+        startActivity(intent);
+        finish();
+        //*/
     }
 }
