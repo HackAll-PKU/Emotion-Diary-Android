0845f4ba0540a569b55b962a562ad489551b314d
Rush66try <liuzhengyuan1995@foxmail.com>
2016-06-06 20:24:18 +0800

    主界面更新

--- app/src/main/java/org/hackpku/emotiondiary/Homepage/CalendarView.java
+++ app/src/main/java/org/hackpku/emotiondiary/Homepage/CalendarView.java
@@ -7,9 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.*;
 import org.hackpku.emotiondiary.R;
+import org.hackpku.emotiondiary.common.Diary.Diary;
+import org.hackpku.emotiondiary.common.Diary.DiaryHelper;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
+import io.realm.RealmResults;
 
 /**
  * Created by Liu Zhengyuan on 2016/5/31.
@@ -21,18 +24,22 @@ public class CalendarView  extends FrameLayout {
     private TextView tv_week;  //显示当天的星期
     private TextView tv_today;  //"今天"按钮
     private MonthDateView monthDateView;  //月历视图
+    private DiaryHelper diaryHelper=new DiaryHelper();
 
     public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.calendar, this);
-        List<Integer> hasThingList = new ArrayList<Integer>();  //记录需要标记日期的List，可用来表示当天有日记的日期
         iv_left = (ImageView) findViewById(R.id.iv_left);
         iv_right = (ImageView) findViewById(R.id.iv_right);
         monthDateView = (MonthDateView) findViewById(R.id.monthDateView);
         tv_date = (TextView) findViewById(R.id.date_text);
-        tv_week  =(TextView) findViewById(R.id.week_text);
+        tv_week = (TextView) findViewById(R.id.week_text);
         tv_today = (TextView) findViewById(R.id.tv_today);
-        monthDateView.setTextView(tv_date,tv_week);
+        monthDateView.setTextView(tv_date, tv_week);
+
+        int year = monthDateView.getmSelYear();
+        int month = monthDateView.getmSelMonth();
+        List<Integer> hasThingList = getMonthList(year, month);  //记录需要标记日期的List，可用来表示当天有日记的日期
         monthDateView.setDaysHasThingList(hasThingList);
         setOnlistener();  //初始化monthDateView各按钮点击事件
     }
@@ -43,16 +50,28 @@ public class CalendarView  extends FrameLayout {
     private void setOnlistener(){
         iv_left.setOnClickListener(new View.OnClickListener() {
 
-            @Override
+            @Override  //上个月
             public void onClick(View v) {
+                int year = monthDateView.getmSelYear();
+                int month = monthDateView.getmSelMonth();
+                if (month == 0)
+                    monthDateView.setDaysHasThingList(getMonthList(year - 1, 11));
+                else
+                    monthDateView.setDaysHasThingList(getMonthList(year, month - 1));
                 monthDateView.onLeftClick();
             }
         });
 
         iv_right.setOnClickListener(new View.OnClickListener() {
 
-            @Override
+            @Override  //下个月
             public void onClick(View v) {
+                int year = monthDateView.getmSelYear();
+                int month = monthDateView.getmSelMonth();
+                if (month == 11)
+                    monthDateView.setDaysHasThingList(getMonthList(year + 1, 0));
+                else
+                    monthDateView.setDaysHasThingList(getMonthList(year, month + 1));
                 monthDateView.onRightClick();
             }
         });
@@ -61,9 +80,27 @@ public class CalendarView  extends FrameLayout {
 
             @Override
             public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int currYear = calendar.get(Calendar.YEAR);
+                int currMonth = calendar.get(Calendar.MONTH);
+                monthDateView.setDaysHasThingList(getMonthList(currYear, currMonth));
                 monthDateView.setTodayToView();
             }
         });
+    }
+
+    /**
+     * 获取指定月份每天的日记数
+     */
+    private List<Integer> getMonthList(int year,int month) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 1; i <= getMonthDays(year, month); i++) {  //对每一天
+            GregorianCalendar date = new GregorianCalendar();
+            date.set(year, month+1, i);
+            RealmResults<Diary> diaries = diaryHelper.getDiariesOfDay(date);
+            list.add(diaries.size());
+        }
+        return list;
     }
 
     /**
@@ -72,6 +109,10 @@ public class CalendarView  extends FrameLayout {
      */
     public void setDateClick(MonthDateView.DateClick dataClick){
         monthDateView.setDateClick(dataClick);
+    }
+
+    public void setList(List<Integer> list){
+        monthDateView.setDaysHasThingList(list);
     }
 
     /**
@@ -96,5 +137,18 @@ public class CalendarView  extends FrameLayout {
      */
     public int getSelDay(){
         return monthDateView.getmSelDay();
+    }
+
+    /**
+     * 返回某年某月有几天
+     * @param year
+     * @param month
+     * @return
+     */
+    private int getMonthDays(int year, int month) {
+        int[] day = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+            day[1] += 1;
+        return day[month];
     }
 }
