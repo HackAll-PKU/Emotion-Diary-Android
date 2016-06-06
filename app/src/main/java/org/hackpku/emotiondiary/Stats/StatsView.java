fe27ae9056a7d625824a19783b559dc71a40eb60
YutingKou <yutingkou@pku.edu.cn>
2016-06-06 19:59:22 +0800

    加了chart一部分

--- app/src/main/java/org/hackpku/emotiondiary/Stats/StatsView.java
+++ app/src/main/java/org/hackpku/emotiondiary/Stats/StatsView.java
@@ -1,7 +1,37 @@
 package org.hackpku.emotiondiary.Stats;
 
+import android.content.Context;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.*;
+
+import com.github.mikephil.charting.charts.LineChart;
+import org.hackpku.emotiondiary.R;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by kouyuting on 2016/6/6.
  */
-public class StatsView {
+public class StatsView extends FrameLayout {
+    private Button thisWeek;//显示这周心情
+    private Button thisMonth;//显示这月心情
+
+    public StatsView(Context context , AttributeSet attrs){
+        super(context,attrs);
+
+
+
+    }
+
+
+
+    LineChart chart = (LineChart) findViewById(R.id.chart);
+
+
+
 }
