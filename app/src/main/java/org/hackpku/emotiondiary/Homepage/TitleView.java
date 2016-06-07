ab1ede724ffc3ae59a01ea5e8e35cb36ab962c97
Zac Chan <sunshinecltzac@gmail.com>
2016-06-07 17:35:02 +0800

    基本逻辑

--- app/src/main/java/org/hackpku/emotiondiary/Homepage/TitleView.java
+++ app/src/main/java/org/hackpku/emotiondiary/Homepage/TitleView.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.util.AttributeSet;
-import android.view.*;
-import android.widget.*;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import org.hackpku.emotiondiary.R;
-import org.hackpku.emotiondiary.Stats.ICalendarView;
 import org.hackpku.emotiondiary.Stats.StatsActivity;
-import org.hackpku.emotiondiary.Stats.StatsView;
 
 /**
  * Created by Liu Zhengyuan on 2016/5/30.
@@ -20,10 +21,9 @@ public class TitleView extends FrameLayout {
     private Button btnTimeline;
     private TextView titleText;
     private Activity HomePageActivity;
-    private ICalendarView calendarView;
 
 
-    public TitleView(Context context, AttributeSet attrs) {
+    public TitleView(final Context context, AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.title, this);
         titleText = (TextView) findViewById(R.id.title_text);
@@ -43,9 +43,8 @@ public class TitleView extends FrameLayout {
             public void onClick(View v) {
                 //调出心情统计页面
                 Intent intent = new Intent();
-                intent.setClass(HomePageActivity,StatsActivity.class);
-                HomePageActivity.startActivity(intent);
-                calendarView.onStatsView();
+                intent.setClass(context,StatsActivity.class);
+                context.startActivity(intent);
             }
         });
 
--- app/src/main/java/org/hackpku/emotiondiary/Stats/StatsActivity.java
+++ app/src/main/java/org/hackpku/emotiondiary/Stats/StatsActivity.java
@@ -5,8 +5,17 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
 import org.hackpku.emotiondiary.R;
 import org.hackpku.emotiondiary.common.Diary.DiaryHelper;
+
+import java.util.ArrayList;
 
 /**
  * Created by lenovo on 2016/6/7.
@@ -14,39 +23,69 @@ import org.hackpku.emotiondiary.common.Diary.DiaryHelper;
 public class StatsActivity extends FragmentActivity {
     private Button thisWeek;//显示这周心情
     private Button thisMonth;//显示这月心情
-    private StatsView statsView;
+    private LineChart chart;
     private boolean isThisWeek;
 
     /**
-     *显示页面
+     * 显示页面
      */
-    protected void onCreate(Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        statsView =(StatsView) findViewById(R.id.chart);
+        setContentView(R.layout.activity_stats);
+        chart = (LineChart) findViewById(R.id.chart);
         thisWeek = (Button) findViewById(R.id.Week);
         thisMonth = (Button) findViewById(R.id.Month);
         this.isThisWeek = true;
         setOnlistener();//初始化button的监听器
-        statsView.showData();
+        showData();
     }
+
     /**
      * 初始化thisWeek和thisMonth按钮点击事件
      */
-
-    public void setOnlistener(){
-        thisWeek.setOnClickListener(new View.OnClickListener(){
+    private void setOnlistener() {
+        thisWeek.setOnClickListener(new View.OnClickListener() {
             @Override  //一周
             public void onClick(View v) {
                 isThisWeek = true;
+                showData();
             }
         });
-        thisMonth.setOnClickListener(new View.OnClickListener(){
+        thisMonth.setOnClickListener(new View.OnClickListener() {
             @Override  //一月
             public void onClick(View v) {
                 isThisWeek = false;
+                showData();
             }
         });
     }
 
+    private void showData() {
+        DiaryHelper diaryHelper = new DiaryHelper();
+        int date;
+        //过去一个月的统计值心情，不在乎1天2天的
+        date = isThisWeek ? 7 : 30;
+        ArrayList<Double> emotionsRowData = diaryHelper.getHappinessForTimeAsArrayList(date);
+
+        //添加数据绘图
+        ArrayList<Entry> emotions = new ArrayList<>();
+        ArrayList<String> xVals = new ArrayList<>();
+        for (int loop = 0; loop < date; ++loop) {
+            Entry c1 = new Entry(emotionsRowData.get(loop).floatValue(), loop);
+            emotions.add(c1);
+            xVals.add("");
+        }
+        LineDataSet setEmotions = new LineDataSet(emotions, "My Emotions");
+        setEmotions.setAxisDependency(YAxis.AxisDependency.LEFT);
+
+        ArrayList<ILineDataSet> dataSets = new ArrayList<>();
+        dataSets.add(setEmotions);
+
+
+        LineData data = new LineData(xVals, dataSets);
+        chart.setData(data);
+        chart.invalidate(); // refresh
+
+    }
 
 }
